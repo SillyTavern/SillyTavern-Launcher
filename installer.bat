027d31fe795397bd1104491f0d72ef1872af87a2
deffcolony <61471128+deffcolony@users.noreply.github.com>
2024-05-02 18:11:49 +0200

    Update installer.bat

--- installer.bat
+++ installer.bat
@@ -319,20 +319,20 @@ chcp 65001 > nul
 
 REM Confirm with the user before proceeding
 echo.
-echo %blue_bg%╔════ INSTALL SUMMARY ══════════════════════════════════════════════════════════════════════════╗%reset%
-echo %blue_bg%║ Extras has been discontinued since April 2024 and won't receive any new updates or modules.   ║%reset%
-echo %blue_bg%║ The vast majority of modules are available natively in the main SillyTavern application.      ║%reset%
-echo %blue_bg%║ You may still install and use it but don't expect to get support if you face any issues.      ║%reset%
-echo %blue_bg%║ Below is a list of package requirements that will get installed:                              ║%reset%
-echo %blue_bg%║ * SillyTavern-extras [Size: 65 MB]                                                            ║%reset%
-echo %blue_bg%║ * Visual Studio BuildTools 2022 [Size: 3,10 GB]                                               ║%reset%
-echo %blue_bg%║ * Miniconda3 [INSTALLED] [Size: 630 MB]                                                       ║%reset%
-echo %blue_bg%║ * Miniconda3 env - extras [Size: 9,98 GB]                                                     ║%reset%
-echo %blue_bg%║ * Git [INSTALLED] [Size: 338 MB]                                                              ║%reset%
-echo %blue_bg%║ * Microsoft Visual C++ 2015-2022 Redistributable (x64) [Size: 20,6 MB]                        ║%reset%
-echo %blue_bg%║ * Microsoft Visual C++ 2015-2022 Redistributable (x86) [Size: 18 MB]                          ║%reset%
-echo %blue_bg%║ TOTAL INSTALL SIZE: 13,67 GB                                                                  ║%reset%
-echo %blue_bg%╚═══════════════════════════════════════════════════════════════════════════════════════════════╝%reset%
+echo %red_bg%╔════ INSTALL SUMMARY ══════════════════════════════════════════════════════════════════════════╗%reset%
+echo %red_bg%║ Extras has been DISCONTINUED since April 2024 and WON'T receive any new updates or modules.   ║%reset%
+echo %red_bg%║ The vast majority of modules are available natively in the main SillyTavern application.      ║%reset%
+echo %red_bg%║ You may still install and use it but DON'T expect to get support if you face any issues.      ║%reset%
+echo %red_bg%║ Below is a list of package requirements that will get installed:                              ║%reset%
+echo %red_bg%║ * SillyTavern-extras [Size: 65 MB]                                                            ║%reset%
+echo %red_bg%║ * Visual Studio BuildTools 2022 [Size: 3,10 GB]                                               ║%reset%
+echo %red_bg%║ * Miniconda3 [INSTALLED] [Size: 630 MB]                                                       ║%reset%
+echo %red_bg%║ * Miniconda3 env - extras [Size: 9,98 GB]                                                     ║%reset%
+echo %red_bg%║ * Git [INSTALLED] [Size: 338 MB]                                                              ║%reset%
+echo %red_bg%║ * Microsoft Visual C++ 2015-2022 Redistributable (x64) [Size: 20,6 MB]                        ║%reset%
+echo %red_bg%║ * Microsoft Visual C++ 2015-2022 Redistributable (x86) [Size: 18 MB]                          ║%reset%
+echo %red_bg%║ TOTAL INSTALL SIZE: 13,67 GB                                                                  ║%reset%
+echo %red_bg%╚═══════════════════════════════════════════════════════════════════════════════════════════════╝%reset%
 echo.
 set /p "confirmation=Are you sure you want to proceed? [Y/N]: "
 if /i "%confirmation%"=="Y" (
