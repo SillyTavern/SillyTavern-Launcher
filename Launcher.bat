f4f1561361e9af23aea4516e99711f484ba890a7
Blueprint Coding <hayden@blueprintcoders.com>
2024-07-06 18:15:34 -0600

    Forgot to update version number...

--- Launcher.bat
+++ Launcher.bat
@@ -13,7 +13,7 @@ REM Issues: https://github.com/SillyTavern/SillyTavern-Launcher/issues
 title STL [STARTUP CHECK]
 setlocal
 
-set "stl_version=V1.2.0"
+set "stl_version=V1.2.1"
 set "stl_title_pid=STL [TROUBLESHOOTING]"
 
 REM ANSI Escape Code for Colors
