1b23aafb07021205af2bc487729932acb9db88e5
Blueprint Coding <hayden@blueprintcoders.com>
2024-09-04 16:23:11 -0600

    Fix issue for tailscale install

--- bin/functions/Toolbox/App_Installer/Core_Utilities/install_tailscale.bat
+++ bin/functions/Toolbox/App_Installer/Core_Utilities/install_tailscale.bat
@@ -1,23 +1,48 @@
 @echo off
 setlocal enabledelayedexpansion
+
+rem Define color scheme
+set blue_bg=[\033[44m
+set reset=[\033[0m
+set blue_fg_strong=[\033[34m
+set green_fg_strong=[\033[32m
+set red_fg_strong=[\033[31m
+set time=%time:~0,8%
+
 :install_tailscale
 title STL [INSTALL-TAILSCALE]
-set log_dir = %log_dir%
+set log_dir=%log_dir%
 
 echo %blue_bg%[%time%]%reset% %blue_fg_strong%[INFO]%reset% Do you already have a Tailscale account set up? (Y/N)
 set /p tailscale_account="Answer (Y/N): "
 
 if /i "%tailscale_account%"=="Y" (
-    echo %blue_bg%[%time%]%reset% %blue_fg_strong%[INFO]%reset% Running Tailscale configuration...
-    call %app_installer_core_utilities_dir%\config_tailscale.bat
+    echo %blue_bg%[%time%]%reset% %blue_fg_strong%[INFO]%reset% Installing Tailscale...
+    winget install Tailscale.Tailscale
+
+    if %errorlevel%==0 (
+        echo %blue_bg%[%time%]%reset% %blue_fg_strong%[INFO]%reset% %green_fg_strong%Tailscale installed successfully.%reset%
+        echo %blue_bg%[%time%]%reset% %blue_fg_strong%[INFO]%reset% Running Tailscale configuration...
+        call %app_installer_core_utilities_dir%\config_tailscale.bat
+    ) else (
+        echo %blue_bg%[%time%]%reset% %blue_fg_strong%[ERROR]%reset% %red_fg_strong%Tailscale installation failed.%reset%
+    )
 ) else (
     echo %blue_bg%[%time%]%reset% %blue_fg_strong%[INFO]%reset% Opening Tailscale sign-up page...
     start "" "https://login.tailscale.com/start"
-    echo %blue_bg%[%time%]%reset% %blue_fg_strong%[INFO]%reset% Press any key after signing up to continue configuration...
+    echo %blue_bg%[%time%]%reset% %blue_fg_strong%[INFO]%reset% Press any key after signing up to continue installation...
     pause
 
-    echo %blue_bg%[%time%]%reset% %blue_fg_strong%[INFO]%reset% Running Tailscale configuration...
-    call config_tailscale.bat
+    echo %blue_bg%[%time%]%reset% %blue_fg_strong%[INFO]%reset% Installing Tailscale...
+    winget install Tailscale.Tailscale
+
+    if %errorlevel%==0 (
+        echo %blue_bg%[%time%]%reset% %blue_fg_strong%[INFO]%reset% %green_fg_strong%Tailscale installed successfully.%reset%
+        echo %blue_bg%[%time%]%reset% %blue_fg_strong%[INFO]%reset% Running Tailscale configuration...
+        call %app_installer_core_utilities_dir%\config_tailscale.bat
+    ) else (
+        echo %blue_bg%[%time%]%reset% %blue_fg_strong%[ERROR]%reset% %red_fg_strong%Tailscale installation failed.%reset%
+    )
 )
 
 pause
