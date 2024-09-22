bf1d620d299ad982f3edc994a1a58297d703a23e
deffcolony <61471128+deffcolony@users.noreply.github.com>
2024-09-22 18:53:33 +0200

    Update install_cudatoolkit.bat

--- bin/functions/Toolbox/App_Installer/Core_Utilities/install_cudatoolkit.bat
+++ bin/functions/Toolbox/App_Installer/Core_Utilities/install_cudatoolkit.bat
@@ -1,15 +1,17 @@
 @echo off
 
 :install_cudatoolkit
-REM Check if file exists
-if not exist "%bin_dir%\cuda_12.5.1_windows_network.exe" (
-    curl -L -o "%bin_dir%\cuda_12.5.1_windows_network.exe" "https://developer.download.nvidia.com/compute/cuda/12.5.1/network_installers/cuda_12.5.1_windows_network.exe"
-) else (
-    echo %blue_bg%[%time%]%reset% %blue_fg_strong%[INFO] "cuda_12.5.1_windows_network.exe" file already exists.%reset%
+REM Check if file exists and remove it if it does
+if exist "%bin_dir%\cuda_12.6.1_windows_network.exe" (
+    echo %blue_bg%[%time%]%reset% %blue_fg_strong%[INFO] Removing existing "cuda_12.6.1_windows_network.exe".%reset%
+    del "%bin_dir%\cuda_12.6.1_windows_network.exe"
 )
 
+REM Download the CUDA installer
+curl -L -o "%bin_dir%\cuda_12.6.1_windows_network.exe" "https://developer.download.nvidia.com/compute/cuda/12.6.1/network_installers/cuda_12.6.1_windows_network.exe"
+
 echo %blue_bg%[%time%]%reset% %blue_fg_strong%[INFO]%reset% Installing CUDA Toolkit...
-start "" "%bin_dir%\cuda_12.5.1_windows_network.exe" visual_studio_integration_12.5.1
+start "" "%bin_dir%\cuda_12.6.1_windows_network.exe" visual_studio_integration_12.6.1
 echo %blue_bg%[%time%]%reset% %blue_fg_strong%[INFO]%reset% %green_fg_strong%When install is finished:%reset%
 
 REM If CUDA Toolkit fails to install then copy all files from MSBuildExtensions into BuildCustomizations
