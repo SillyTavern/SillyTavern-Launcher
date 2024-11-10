ad5193eb84eeca5c58c6e1b9261a79aab5571f36
deffcolony <61471128+deffcolony@users.noreply.github.com>
2024-11-10 20:55:00 +0100

    use node.lts

--- bin/functions/Toolbox/App_Installer/Core_Utilities/install_nodejs.bat
+++ bin/functions/Toolbox/App_Installer/Core_Utilities/install_nodejs.bat
@@ -3,7 +3,7 @@
 :install_nodejs
 title STL [INSTALL NODEJS]
 echo %blue_bg%[%time%]%reset% %blue_fg_strong%[INFO]%reset% Installing Node.js...
-winget install -e --id OpenJS.NodeJS
+winget install -e --id OpenJS.NodeJS.LTS
 echo %blue_bg%[%time%]%reset% %blue_fg_strong%[INFO]%reset% %green_fg_strong%Node.js is installed.%reset%
 
 REM Prompt user to restart
--- bin/functions/Toolbox/App_Uninstaller/Core_Utilities/uninstall_nodejs.bat
+++ bin/functions/Toolbox/App_Uninstaller/Core_Utilities/uninstall_nodejs.bat
@@ -3,6 +3,6 @@
 :uninstall_nodejs
 title STL [UNINSTALL-NODEJS]
 echo %blue_bg%[%time%]%reset% %blue_fg_strong%[INFO]%reset% Uninstalling Node.js...
-winget uninstall --id OpenJS.NodeJS
+winget uninstall --id OpenJS.NodeJS.LTS
 echo %blue_bg%[%time%]%reset% %blue_fg_strong%[INFO]%reset% %green_fg_strong%Node.js has been uninstalled successfully.%reset%
 goto :app_uninstaller_core_utilities
